87232dd52588f3c44e9f34775ae156c6e2323d35
tylerwhyler <tyjiman@gmail.com>
2020-08-08 21:53:26 -0700

    refresher on array and object destructuring

--- first-app.js
+++ first-app.js
@@ -1,10 +1,16 @@
-// const person = {
-//     name: "Tyler",
-//     age: 22,
-//     greet() {
-//         console.log("hi, im a function of an object and my name is " + this.name)
-//     }
-// }
+const person = {
+    name: "Tyler",
+    age: 22,
+    greet() {
+        return ("hi, im a function of an object and my name is " + this.name)
+    }
+}
+
+const printName = ({ name, greet }) => {
+    console.log(name, greet());
+}
+
+printName(person);
 
 const hobbies = ['sports', 'games'];
 
@@ -22,11 +28,11 @@ hobbies.push("scripting js");
 // console.log(hobbies.map(hobby => hobby))
 // console.log(hobbies)
 
-const copiedArray = [...hobbies]
-console.log(copiedArray)
+// const copiedArray = [...hobbies]
+// console.log(copiedArray)
 
-const toArray = (...args) => {
-    return args;
-}
+// const toArray = (...args) => {
+//     return args;
+// }
 
-console.log(toArray(1, 2, 3, 4, 5))
+// console.log(toArray(1, 2, 3, 4, 5))
