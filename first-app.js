30edaca1e9e2b1bc0cb60ad6da7995a652ad0abc
tylerwhyler <tyjiman@gmail.com>
2020-08-08 21:04:54 -0700

    big difference between for in's and for of's, for in an array refers to the index value, not the actual value

--- first-app.js
+++ first-app.js
@@ -1,9 +1,17 @@
-const person = {
-    name: "Tyler",
-    age: 22,
-    greet() {
-        console.log("hi, im a function of an object and my name is " + this.name)
-    }
+// const person = {
+//     name: "Tyler",
+//     age: 22,
+//     greet() {
+//         console.log("hi, im a function of an object and my name is " + this.name)
+//     }
+// }
+
+const hobbies = ['sports', 'games'];
+
+for (let hobby in hobbies) {
+    console.log(hobby)
 }
 
-person.greet();
+for (let hobby of hobbies) {
+    console.log(hobby)
+}
