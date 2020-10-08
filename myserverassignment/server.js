df4d8023ab5de149c5fd9d41fb3aa576f99368e3
tylerwhyler <tyjiman@gmail.com>
2020-10-08 05:11:25 -0700

    refresher on adding custom scripts to package.json, and initializing the npm project altogether

--- myserverassignment/server.js
+++ myserverassignment/server.js
@@ -9,11 +9,16 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.setHeader('Content-type', 'text/html');
         res.write('<html>');
+        res.write(
+            '<head><style>html {background-color: rgb(75, 75, 75);}</style></head>'
+        );
+        res.write('<body>');
         res.write('<h1>Root route hellooos</h1>');
         res.write('<form action="/users" method="POST">');
         res.write("<input type='text' name='username' />");
         res.write("<input type='submit' />");
         res.write('</form>');
+        res.write('</body>');
         res.write('</html>');
         return res.end();
     }
